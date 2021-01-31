439f785aa876a4611b3d1bb98cf519f62461241d
Zimian Xu <zimian.xu@thoughtworks.com>
2021-01-31 23:04:49 +0800

    add  process command and request url feature

--- docker/network_drivers/bridge/app_code/http_server.py
+++ docker/network_drivers/bridge/app_code/http_server.py
@@ -1,19 +1,39 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+import urllib.request
+import tempfile
+import shutil
+import sys
+import getopt
 
-data = {'result': 'this is a test'}
-host = ('localhost', 8080)
+data = {'result': 'hello world'}
+host = ("localhost", 8080)
+
 
 class Resquest(BaseHTTPRequestHandler):
     def do_GET(self):
+        url = self.processCommand()
+        file = urllib.request.urlopen(url)
+        html = file.read()
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/html;charset=utf-8')
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode())
-        self.wfile.writelines(bytes(["hello","world"]))
+        self.wfile.write(html)
+        file.close()
 
-    
+    def processCommand(self):
+        comandArgStrs = sys.argv[1:]
+        comandArgTuples, others = getopt.getopt(comandArgStrs, "u:")
+        for key, value in comandArgTuples:
+            if key in ['-u']:
+                return value
+        return "https://www.baidu.com/"
+
+
 if __name__ == '__main__':
     server = HTTPServer(host, Resquest)
     print("Starting server, listen at: %s:%s" % host)
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit(0)
