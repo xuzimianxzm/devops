f5ba38ecf32e87fbfab87268bc7a243b49c55074
516654313@qq.com <516654313@qq.com>
2021-02-21 14:51:38 +0800

    refactor the shell module is mainly  add common.sh and used it

---
+++ docker/network_drivers/bridge/code/shell/common.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+function runDockerFile() {
+    checkImages=$(docker images python_web)
+    if [[ $checkImages =~ 'python_web' ]]; then
+        echo "=> python_web image existed then removed the image and rebuild it:"
+        echo "=> removed the containers:" $(docker rm -f python_web)
+        echo "=> removed the images:" $(docker rmi -f python_web)
+    else
+        echo "=> python_web image dose not existed then build the image:"
+    fi
+
+    echo $(docker build --force-rm -t python_web -f docker_file/python_web/Dockerfile .)
+
+    if [[ $checkImages =~ 'python_web' ]]; then
+        echo "=> python_web build succeed and then run the image:"
+    else
+        echo "=> python_web build failed and then exit!"
+        exit -1
+    fi
+
+    echo "=>" $(docker run -it --name python_web -p 8086:8080/tcp -d python_web)
+}
---
+++ docker/network_drivers/bridge/python_web_dockerfile_run.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Enter the current directory where the shell is located
+cd `dirname $0`
+
+# import common.sh 
+source code/shell/common.sh
+
+# excute function
+runDockerFile
