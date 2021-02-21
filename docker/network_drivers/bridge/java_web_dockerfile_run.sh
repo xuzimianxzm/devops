c8768b4eb539aee136a35180901ad5408230428c
516654313@qq.com <516654313@qq.com>
2021-02-21 16:09:42 +0800

    refactor: made java_web_dockerfile_run.sh used the refactor common.sh

--- docker/network_drivers/bridge/java_web_dockerfile_run.sh
+++ docker/network_drivers/bridge/java_web_dockerfile_run.sh
@@ -1,21 +1,10 @@
 #! /bin/bash
 
-checkImages=$(docker images java_web)
-if [[ $checkImages =~ 'java_web' ]]; then
-    echo "=> java_web image existed then removed the image and rebuild it:"
-    echo "=> removed the containers:" $(docker rm -f java_web)
-    echo "=> removed the images:" $(docker rmi -f java_web)
-else
-    echo "=> java_web image dose not existed then build the image:"
-fi
+# Enter the current directory where the shell is located
+cd `dirname $0`
 
-echo $(docker build --force-rm -t java_web -f docker_file/java_web/Dockerfile .)
+# import common.sh 
+source code/shell/common.sh
 
-if [[ $checkImages =~ 'java_web' ]]; then
-    echo "=> java_web build succeed and then run the image:"
-else
-    echo "=> java_web build failed and then exit!"
-    exit -1
-fi
-
-echo "=>" $(docker run -it --name java_web -p 8080:8080/tcp -d java_web)
+# excute function
+runDockerFile java_web 8080
